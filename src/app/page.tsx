aaedec46872e0136c20911de3de78af13a5add0d
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2025-11-25 20:05:15 +0000

    there is almost no space above the education and certifications section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <Skills />
+        <Skills className="px-4 py-16 sm:px-6 sm:py-24 lg:px-8" />
         <Experience />
-        <Projects />
-        <Education />
-        <Contact />
+        <Projects className="px-4 py-16 sm:px-6 sm:py-24 lg:px-8" />
+        <Education className="px-4 py-16 sm:px-6 sm:py-24 lg:px-8" />
+        <Contact className="px-4 py-16 sm:px-6 sm:py-24 lg:px-8" />
       </main>
       <Footer />
     </div>
